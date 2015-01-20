f7e16573d83d7215edb5808965d5e1c18fc07349
xiaoxing <dawnstar.hu@gmail.com>
2015-01-20 20:48:49 +1300

    bug fixed

--- install.zsh
+++ install.zsh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 
 setopt EXTENDED_GLOB
-for rcfile in "$(pwd -P)"/runcoms/^README.md(.N); do
+
+home="${ZDOTDIR:-$HOME}/.zprezto"
+if [[ -a "$home" ]]; then
+  rm $home
+fi
+
+ln -s "$(pwd -P)" "$home"
+
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   target="${ZDOTDIR:-$HOME}/.${rcfile:t}"
   if [[ -h "$target" || -a "$target" ]]; then
     rm "$target"

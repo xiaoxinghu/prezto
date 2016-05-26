f1c1b091555adb09022c994fb2f21c488f747031
Xiaoxing Hu <dawnstar.hu@gmail.com>
2016-05-26 12:42:46 +1200

    sync added

---
+++ sync.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+upstream="sorin-ionescu/prezto"
+
+remotes=("${(@f)$(git remote)}")
+
+if [[ ${remotes[(r)upstream]} != upstream ]]; then
+   git remote add upstream "https://github.com/"$upstream
+fi
+
+git fetch upstream
+git checkout master
+git merge upstream/master
